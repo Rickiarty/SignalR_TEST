d5b9f745d5e8b6d260cba63e294926bb2f6ef8d5
Rickiarty <fatalframe0719@gmail.com>
2024-03-28 18:02:28 +0800

    1st time upload

---
+++ SignalR_TEST/Hubs/ChatHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
+
+namespace SignalR_TEST.Hubs
+{
+    internal class ChatHub : Hub
+    {
+        private static ConcurrentDictionary<string, string> _onlineUsers = new ConcurrentDictionary<string, string>();
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            _onlineUsers.TryAdd(Context.ConnectionId, Context.ConnectionId);
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            string connectionId;
+            _onlineUsers.TryRemove(Context.ConnectionId, out connectionId!);
+            await base.OnDisconnectedAsync(exception);
+        }
+
+    }
+}
---
+++ SignalR_TEST/Program.cs
@@ -0,0 +1,30 @@
+using SignalR_TEST.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting()
+    .UseAuthorization()
+    .UseEndpoints(endpoints =>
+    {
+        _ = endpoints.MapHub<ChatHub>("/ChatHub");
+        _ = endpoints.MapRazorPages();
+    });
+
+app.Run();
